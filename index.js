54f9ba163436760f114078b6d829295016a0c544
denniskimotho <denniskimotho222@gmail.com>
2023-09-29 23:48:04 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = (name) =>{
+    cats.push(name)
+}
+
+const destructivelyPrependCat = (name) =>{
+    cats.unshift(name)
+}
+
+const destructivelyRemoveLastCat = function(){
+    cats.pop()
+}
+const destructivelyRemoveFirstCat = () =>{
+    cats.shift()
+}
+
+function appendCat(name){
+         
+      const newCats = [
+        ...cats.slice(0, 1, 2),
+        name
+      ];
+      
+    return newCats;
+}
+
+const prependCat=(name)=>{
+
+}
+
+const removeLastCat = () =>{
+    let newCats = cats
+
+    return newCats.pop()
+}
+
+const removeFirstCat = ()=>{
+    let newCats = cats
+    return newCats.shift()
+}
